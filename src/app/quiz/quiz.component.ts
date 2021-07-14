8fa7c231e679b6a9264f898a85da60bc93c8bcce
naro36 <47540109+vakho-naro@users.noreply.github.com>
2021-07-15 01:59:38 +0400

    end
    finish

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -15,35 +15,28 @@ export class QuizComponent implements OnInit {
 
   difficultyHandler(event: any) {
     this.difficulty = event.target.value
-    console.log(this.difficulty)
   }
   categoryHandler(event: any) {
     this.category = event.target.value
-    console.log(this.category)
   }
   getUrl() {
      if(this.category == "any" && this.difficulty == "any") {
       this.quiz.getAny().subscribe((result) => {
-        console.log(result)
         this.questionData = result;
         this.display = false
       })
-      console.log("result")
     }else if(this.difficulty == "any") {
       this.quiz.getDifficyltyAny(this.category).subscribe((result) => {
-        console.log(result)
         this.questionData = result;
         this.display = false
       })
     } else if(this.category == "any") {
       this.quiz.getTypeAny(this.difficulty).subscribe((result) => {
-        console.log(result)
         this.questionData = result;
         this.display = false
       })
     } else {
       this.quiz.getQuiz(this.difficulty, this.category).subscribe((result) => {
-        console.log(result)
         this.questionData = result;
         this.display = false
       })
